aca98de262aff75e4b0a53c22825d1a82f8cd070
Kim Collins <kim@kimcollins.ca>
2017-04-09 19:24:35 -0400

    section 5 up to Testing

--- src/server/index.js
+++ src/server/index.js
@@ -6,6 +6,8 @@ import { APP_NAME, STATIC_PATH, WEB_PORT } from '../shared/config';
 import { isProd } from '../shared/util';
 import renderApp from './render-app';
 
+import { helloEndpointRoute } from '../shared/routes';
+
 const app = express();
 
 app.use(compression());
@@ -16,6 +18,9 @@ app.get('/', (req, res) => {
 	res.send(renderApp(APP_NAME));
 });
 
+app.get(helloEndpointRoute(), (req, res) => {
+	res.json({ serverMessage: `Hello from the server! (received ${req.params.num})` });
+});
 app.listen(WEB_PORT, () => {
 	// eslint-disable-next-line no-console
 	console.log(`Server running on port ${WEB_PORT} ${isProd ? '(production)' : '(development).\nKeep"yarn dev:wds" running in an other terminal'}`);
